c23d8cae0a12c4f67627214e0152a7ae25d2094f
bogoddr <bogoddr@gmail.com>
2023-01-19 21:28:40 -0500

    testing arbitrary arrows input

---
+++ Mimi's Microgames/README.txt
@@ -0,0 +1,6 @@
+by mimi
+
+wip
+
+help:
+- f3 + 8 to show lua errors
--- Mimi's Microgames/default.lua
+++ Mimi's Microgames/default.lua
@@ -5,7 +5,50 @@ return Def.ActorFrame{
    -- keep-alive Actor
    -- this will allow the file to persist for 999 seconds
    -- (or, until the end of the stepchart)
-   Def.Actor{ OnCommand=function(self) self:sleep(999) end },
+   Def.Actor{
+      OnCommand=function(self)
+         self:sleep(999) 
+      end,
+      JudgmentMessageCommand=function(self, params)
+         -- Note that JudgementMessages will be broadcast for any human players,
+         -- but to keep this example simple, we'll limit it to PLAYER_1.
+         if params.Player == PLAYER_1 then
+   
+            -- This SystemMessage will display each judgment in string
+            -- form at the top of the screen as it occurs.
+            -- So, if a note passes and the player misses it,
+            -- "TapNoteScore_Miss"  would be displayed at the
+            -- top of the screen.
+            --
+            -- A W1 judgment (Marvelous in DDR, Fantastic in ITG, etc.)
+            -- would display "TapNoteScore_W1".
+            -- (Note that the "W" is for window, as in "timing window.")
+            --
+            -- Hold notes have their own, separate judgment system.
+            -- So, when a hold note is judged, a JudgmentMessage will be
+            -- broadcast, but the TapNoteScore parameter will be nil.
+            -- Account for that here with a logical or statement that tries
+            -- params.HoldNoteScore if params.TapNoteScore is nil.
+            SCREENMAN:SystemMessage( params.TapNoteScore or params.HoldNoteScore )
+         end
+      end,
+      StepMessageCommand=function(self, params)
+         SCREENMAN:SystemMessage( params.Column )
+         --if ACTIVE and params.PlayerNumber == player then
+
+            --ExtendLine(params.Column + 1)
+            -- print_matrix(incorrect_spots)
+
+            --self:SetNumVertices(#verts)
+               --:SetVertices(verts)
+         --end
+      end,
+   },
+
+   
+
+   --StepMessageCommand=function(self, params)
+   --   SCREENMAN:SystemMessage( params.TapNoteScore or params.HoldNoteScore )
 
    -- the Sprite Actor
    Def.Sprite{
