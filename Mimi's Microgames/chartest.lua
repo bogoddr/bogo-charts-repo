3704df7fde55f8c85ccc65d755161e42a9cada25
zachrispoli <bogoddr@gmail.com>
2025-06-20 17:07:35 -0400

    new stuff

--- Mimi's Microgames/chartest.lua
+++ Mimi's Microgames/chartest.lua
@@ -20,18 +20,25 @@ local main = Def.ActorFrame{
 }
 
 main[#main+1] = Def.Sprite{
+   Texture="bg.png",
+   OnCommand=function(self)
+      self:Center():FullScreen():diffusealpha(1)
+   end
+}
+
+local duration_between_frames = 0.2
+main[#main+1] = Def.Sprite{
     Name="Player1",
-    Texture="wily 6x1.png",
+    Texture="char 5x1.png",
     InitCommand=function(self)
         self.isJumping = false
         self.jumpHeight = 0
         self.jumpVelocity = 0;
-        local duration_between_frames = 0.15
         self:SetStateProperties({
             { Frame=0,  Delay=duration_between_frames},
             { Frame=1,  Delay=duration_between_frames},
-            { Frame=2,  Delay=duration_between_frames},
-            { Frame=3,  Delay=duration_between_frames}
+            { Frame=0,  Delay=duration_between_frames},
+            { Frame=2,  Delay=duration_between_frames}
         })
     end,
     OnCommand=function(self)
@@ -41,22 +48,49 @@ main[#main+1] = Def.Sprite{
         if params.PlayerNumber == 'PlayerNumber_P1' and params.Column == 2 and not self.isJumping then
             self.isJumping = true;
             self.jumpHeight = 0;
-            self.jumpVelocity = 10;
-            self:SetStateProperties({
-                { Frame=0,  Delay=duration_between_frames},
-            })
+            self.jumpVelocity = 50;
         else
             --SM( 'p2 ' .. params.Column )
         end
     end,
     CustomUpdateCommand=function(self)
         self:xy(SCREEN_CENTER_X, SCREEN_CENTER_Y - self.jumpHeight)
-        self.jumpHeight = self.jumpHeight + (self.jumpVelocity * gameState.lastDelta * 10)
-        self.jumpVelocity = self.jumpVelocity - gameState.lastDelta * 20
-        if self.jumpHeight < 0 then
+        if self.isJumping then
+            self:SetStateProperties({
+                { Frame=self.jumpVelocity > 0 and 3 or 4,  Delay=0},
+            })
+            self.jumpHeight = self.jumpHeight + (self.jumpVelocity * gameState.lastDelta * 10)
+            self.jumpVelocity = self.jumpVelocity - gameState.lastDelta * 120
+        end
+        if self.jumpHeight < 0 and self.isJumping then
             self.isJumping = false
             self.jumpHeight = 0
+            self:SetStateProperties({
+                { Frame=0,  Delay=duration_between_frames},
+                { Frame=1,  Delay=duration_between_frames},
+                { Frame=0,  Delay=duration_between_frames},
+                { Frame=2,  Delay=duration_between_frames}
+            })
         end
+    end,
+}
+
+main[#main+1] = Def.Sprite{
+    Name="coachleft",
+    Texture="coach 6x1.png",
+    InitCommand=function(self)
+        local duration_between_frames = 0.2
+        self:SetStateProperties({
+            { Frame=0,  Delay=duration_between_frames},
+            { Frame=1,  Delay=duration_between_frames},
+            { Frame=2,  Delay=duration_between_frames},
+            { Frame=3,  Delay=duration_between_frames},
+            { Frame=4,  Delay=duration_between_frames},
+            { Frame=5,  Delay=duration_between_frames}
+        })
+    end,
+    CustomUpdateCommand=function(self)
+        self:xy(SCREEN_CENTER_X-100, SCREEN_CENTER_Y)
     end,
 }
 
